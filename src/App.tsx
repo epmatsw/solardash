40ecbaadd6cb0325f7c6dd1bb9d79882e250d8e7
Will Stamper <wstamper@gatech.edu>
2022-06-17 08:24:45 -0600

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useMemo, useState } from "react";
+import { Line, LineChart, XAxis, YAxis } from "recharts";
+
+type Data = {
+  watts: number;
+  wattHours: number;
+  date: Date;
+  formattedDate: string;
+};
+
+type Response = {
+  result: {
+    watt_hours: Record<string, number>;
+    watts: Record<string, number>;
+  };
+};
+
+const formatter = new Intl.DateTimeFormat("en-US", {
+  dateStyle: "short",
+  timeStyle: "short",
+});
+
+const publicUrl =
+  "https://api.forecast.solar/estimate/39.80/-105.08/23.39/-15/10";
+const privateUrl =
+  "https://api.forecast.solar/995108ca75752736/estimate/39.80/-105.08/23.39/-15/10";
 
 function App() {
+  const [data, setData] = useState<Data[]>();
+
+  useEffect(() => {
+    (async function () {
+      const fetchResult = await fetch(privateUrl);
+      const { result } = (await fetchResult.json()) as Response;
+
+      const processed: Data[] = [];
+      for (const key in result.watts) {
+        if (!result.watts.hasOwnProperty(key)) continue;
+        if (!result.watt_hours.hasOwnProperty(key)) continue;
+        const date = new Date(key);
+        processed.push({
+          date,
+          wattHours: Math.round(result.watt_hours[key] / 1000),
+          watts: Math.round(result.watts[key] / 1000),
+          formattedDate: formatter.format(date),
+        });
+      }
+
+      setData(processed);
+    })();
+  }, []);
+
+  const maxWatts = useMemo(() => {
+    let max = -Infinity;
+    if (!data) return 0;
+    for (const item of data) {
+      if (item.watts > max) max = item.watts;
+    }
+    return max * 1.5;
+  }, [data]);
+
+  const maxWattHours = useMemo(() => {
+    let max = -Infinity;
+    if (!data) return 0;
+    for (const item of data) {
+      if (item.wattHours > max) max = item.wattHours;
+    }
+    return max * 1.5;
+  }, [data]);
+
+  if (!data) return <div>Loading...</div>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <LineChart width={600} height={300} data={data}>
+        <Line dataKey={"watts"}></Line>
+        <XAxis dataKey={"formattedDate"} />
+        <YAxis max={maxWatts} />
+      </LineChart>
+      <LineChart width={600} height={300} data={data}>
+        <Line dataKey={"wattHours"}></Line>
+        <XAxis dataKey={"formattedDate"} />
+        <YAxis max={maxWattHours} />
+      </LineChart>
+    </>
   );
 }
-
 export default App;
