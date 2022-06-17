cf774fdcb28db73d132316519770d1b6584277f4
Will Stamper <wstamper@gatech.edu>
2022-06-17 08:53:24 -0600

    Make it better

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Body, Equator, Observer } from "astronomy-engine";
 import React, { useEffect, useMemo, useState } from "react";
 import { Line, LineChart, XAxis, YAxis } from "recharts";
 
@@ -5,32 +6,63 @@ type Data = {
   watts: number;
   wattHours: number;
   date: Date;
-  formattedDate: string;
 };
 
 type Response = {
   result: {
     watt_hours: Record<string, number>;
+    watt_hours_day: Record<string, number>;
     watts: Record<string, number>;
   };
 };
 
 const formatter = new Intl.DateTimeFormat("en-US", {
-  dateStyle: "short",
-  timeStyle: "short",
+  day: "numeric",
+});
+const dayFormatter = new Intl.DateTimeFormat("en-US", {
+  weekday: "long",
 });
 
-const publicUrl =
-  "https://api.forecast.solar/estimate/39.80/-105.08/23.39/-15/10";
-const privateUrl =
-  "https://api.forecast.solar/995108ca75752736/estimate/39.80/-105.08/23.39/-15/10";
+const lat = 39.8;
+const long = -105.08;
+const observer = new Observer(lat, long, 0);
+const equ_ofdate = Equator(Body.Sun, new Date(), observer, true, true);
+const dec = equ_ofdate.dec.toFixed(2);
+
+const apiKey =
+  process.env.NODE_ENV === "production"
+    ? new URLSearchParams(window.location.search).get("apiKey")
+    : "995108ca75752736";
+
+const maxKwAC = 7.67;
+const maxKwDC = 9.88;
+const maxKw = maxKwAC;
+
+const publicUrl = `https://api.forecast.solar/estimate/${lat}/${long}/${dec}/-15/${maxKw}`;
+const privateUrl = apiKey
+  ? `https://api.forecast.solar/${apiKey}/estimate/${lat}/${long}/${dec}/-15/${maxKw}`
+  : undefined;
+
+const formatKw = (value: number) => `${Math.round(value / 1000)}kW`;
 
 function App() {
   const [data, setData] = useState<Data[]>();
+  const [days, setDays] = useState<{ date: Date; value: number }[]>();
+  const [api, setApi] = useState<"Public" | "Personal">();
 
   useEffect(() => {
     (async function () {
-      const fetchResult = await fetch(privateUrl);
+      let fetchResult;
+      if (privateUrl) {
+        try {
+          fetchResult = await fetch(privateUrl);
+          setApi("Personal");
+        } catch {}
+      }
+      if (!fetchResult) {
+        fetchResult = await fetch(publicUrl);
+        setApi("Public");
+      }
       const { result } = (await fetchResult.json()) as Response;
 
       const processed: Data[] = [];
@@ -40,13 +72,23 @@ function App() {
         const date = new Date(key);
         processed.push({
           date,
-          wattHours: Math.round(result.watt_hours[key] / 1000),
-          watts: Math.round(result.watts[key] / 1000),
-          formattedDate: formatter.format(date),
+          wattHours: result.watt_hours[key],
+          watts: result.watts[key],
+        });
+      }
+
+      const days = [];
+      for (const key in result.watt_hours_day) {
+        if (!result.watt_hours_day.hasOwnProperty(key)) continue;
+        const date = new Date(key);
+        days.push({
+          date,
+          value: result.watt_hours_day[key],
         });
       }
 
       setData(processed);
+      setDays(days);
     })();
   }, []);
 
@@ -68,21 +110,39 @@ function App() {
     return max * 1.5;
   }, [data]);
 
-  if (!data) return <div>Loading...</div>;
+  if (!data || !days) return <div>Loading...</div>;
 
   return (
-    <>
-      <LineChart width={600} height={300} data={data}>
-        <Line dataKey={"watts"}></Line>
-        <XAxis dataKey={"formattedDate"} />
-        <YAxis max={maxWatts} />
-      </LineChart>
-      <LineChart width={600} height={300} data={data}>
-        <Line dataKey={"wattHours"}></Line>
-        <XAxis dataKey={"formattedDate"} />
-        <YAxis max={maxWattHours} />
-      </LineChart>
-    </>
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-evenly",
+      }}
+    >
+      <span>
+        <LineChart width={600} height={300} data={data}>
+          <Line dataKey={"watts"} dot={false}></Line>
+          <XAxis dataKey={"date"} tickFormatter={formatter.format} />
+          <YAxis max={maxWatts} tickFormatter={formatKw} />
+        </LineChart>
+        <LineChart width={600} height={300} data={data}>
+          <Line dataKey={"wattHours"} dot={false}></Line>
+          <XAxis dataKey={"date"} tickFormatter={formatter.format} />
+          <YAxis max={maxWattHours} tickFormatter={formatKw} />
+        </LineChart>
+        <div style={{ textAlign: "center" }}>{api} API</div>
+      </span>
+      <span>
+        <div>
+          {days.map(({ value, date }) => (
+            <div>
+              {dayFormatter.format(date)}: {formatKw(value)}
+            </div>
+          ))}
+        </div>
+      </span>
+    </div>
   );
 }
 export default App;
