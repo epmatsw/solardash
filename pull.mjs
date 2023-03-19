1893d79e89fea743c7d4fa4c7f8abba0e64d1f64
Will Stamper <wstamper@apple.com>
2023-03-19 10:31:21 -0600

    Make it work with actions

--- pull.mjs
+++ pull.mjs
@@ -1,5 +1,4 @@
 import fs from "fs/promises";
-import { execFileSync } from "child_process";
 
 const numToString = (n) => {
   if (n < 10) {
@@ -9,61 +8,44 @@ const numToString = (n) => {
   }
 };
 
-const update = async () => {
-  const now = new Date();
-  const year = now.getFullYear();
-  const month = now.getMonth() + 1;
-  const date = now.getDate();
-  console.log(`Updating at ${now}`);
+const now = new Date();
+const year = now.getFullYear();
+const month = now.getMonth() + 1;
+const date = now.getDate();
+console.log(`Updating at ${now}`);
 
-  const previousData = JSON.parse(
-    await fs.readFile("./public/data.json", { encoding: "utf8" })
-  );
+const previousData = JSON.parse(
+  await fs.readFile("./public/data.json", { encoding: "utf8" })
+);
 
-  const url = `https://enlighten.enphaseenergy.com/pv/public_systems/2875024/daily_energy?start_date=${year}-${numToString(
-    month
-  )}-${numToString(date)}&end_date=${year}-${numToString(month)}-${numToString(
-    date
-  )}`;
+const url = `https://enlighten.enphaseenergy.com/pv/public_systems/2875024/daily_energy?start_date=${year}-${numToString(
+  month
+)}-${numToString(date)}&end_date=${year}-${numToString(month)}-${numToString(
+  date
+)}`;
 
-  const todaysData = await (await fetch(url)).json();
+const todaysData = await (await fetch(url)).json();
 
-  const datesToReplace = new Map(
-    todaysData.stats.map((s) => [s.start_time, s])
-  );
+const datesToReplace = new Map(todaysData.stats.map((s) => [s.start_time, s]));
 
-  const newData = {
-    stats: previousData.stats
-      .filter(({ production }) => production.some((p) => typeof p === "number"))
-      .map((existingStat) => {
-        if (datesToReplace.has(existingStat.start_time)) {
-          const newStat = datesToReplace.get(existingStat.start_time);
-          datesToReplace.delete(existingStat.start_time);
-          return newStat;
-        } else {
-          return existingStat;
-        }
-      }),
-  };
-
-  newData.stats.push(...Array.from(datesToReplace.values()));
-
-  await fs.writeFile("./public/data.json", JSON.stringify(newData, null, 4), {
-    encoding: "utf8",
-  });
-
-  execFileSync("git", ["add", "./public/data.json"]);
-
-  execFileSync("npm", ["run", "build"]);
-
-  execFileSync("git", ["add", "./docs"]);
-
-  execFileSync("git", ["commit", "--no-gpg-sign", "-n", "-m", `'Update data at ${now}'`]);
-
-  execFileSync("git", ["push"]);
-
-  console.log(`Finished update at ${now}`);
+const newData = {
+  stats: previousData.stats
+    .filter(({ production }) => production.some((p) => typeof p === "number"))
+    .map((existingStat) => {
+      if (datesToReplace.has(existingStat.start_time)) {
+        const newStat = datesToReplace.get(existingStat.start_time);
+        datesToReplace.delete(existingStat.start_time);
+        return newStat;
+      } else {
+        return existingStat;
+      }
+    }),
 };
 
-await update();
-setInterval(update, 15 * 60 * 1000);
+newData.stats.push(...Array.from(datesToReplace.values()));
+
+await fs.writeFile("./public/data.json", JSON.stringify(newData, null, 4), {
+  encoding: "utf8",
+});
+
+console.log(`Finished update at ${now}`);
